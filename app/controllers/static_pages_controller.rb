e1d44a4a8f2eaa6acf571a95853ce43e356a064f
LeeSD90 <lee.doyle90@gmail.com>
2018-05-10 14:30:20 +0200

    Finish project

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,14 @@
 class StaticPagesController < ApplicationController
   require 'flickraw'
+
   def home
-    
+    FlickRaw.api_key = ENV["FLICKR_KEY"]
+    FlickRaw.shared_secret = ENV["FLICKR_SECRET"]
+    @urls = []
+
+    if params[:id]
+      @photos = flickr.photos.search(user_id: params[:id])
+    end
   end
+
 end
